183d46bbb1ed678100e89188628232f85550fb8a
yashverma03 <vermayash2003@gmail.com>
2023-04-02 14:58:59 +0530

    updated PrivateRoute component code

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -1,17 +1,9 @@
 import { Navigate, Outlet } from "react-router-dom";
-import Header from "./header/Header";
 
 const PrivateRoute = ({ isAuthenticated }) => {
   const token = sessionStorage.getItem('accessToken');
 
-  return isAuthenticated && token ? (
-    <>
-      <Header />
-      <Outlet />
-    </>
-  ) : (
-    <Navigate replace to='/account' />
-  );
+  return isAuthenticated && token ? <Outlet /> : <Navigate replace to='/account' />;
 };
 
 export default PrivateRoute;
